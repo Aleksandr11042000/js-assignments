0cb71d2dec5326cf9ba77dae7d762f5962ec0bfd
Aleksandr11042000 <158091115+Aleksandr11042000@users.noreply.github.com>
2026-03-19 16:37:36 +0300

    Update it-optional.js

--- extensions/it-optional.js
+++ extensions/it-optional.js
@@ -1,19 +1,15 @@
-'use strict';
-
-exports = module.exports = testOptional;
-
-function testOptional(title, fn) {
-    
-    it(title, function() {
-        try {
-           fn.call(this);
-        } catch (err) {
-            if (err.message=="Not implemented") {
-                this.test.skip();
-            } else {
-                throw err;
-            }
-        }
-    });
-
-}
+  /**
+   * Returns the result of concatenation of two strings.
+   *
+   * @param {string} value1
+   * @param {string} value2
+   * @return {string}
+   *
+   * @example
+   *   'aa', 'bb' => 'aabb'
+   *   'aa',''    => 'aa'
+   *   '',  'bb'  => 'bb'
+   */
+  function concatenateStrings(value1, value2) {
+     throw new Error('Not implemented');
+  }
